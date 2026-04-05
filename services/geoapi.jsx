0cab02a509e03cff2b6fee9e52a80cc6ae547735
WerbystonLemos <werbyston@hotmial.com>
2026-04-05 02:12:07 -0300

    [Feature] add da lib de icones no react e estruturação e diagramação da página de Home

--- services/geoapi.jsx
+++ services/geoapi.jsx
@@ -4,4 +4,8 @@ const geoapi = axios.create({
     baseURL: 'https://geoapi.info/api/',
 })
 
+export const getFlagUrl = (code) => {
+    return `https://data.geoapi.info/flags/1x1/${code.toLowerCase()}.svg`
+}
+
 export default geoapi;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import geoapi from "../../../services/geoapi";
+import geoapi, { getFlagUrl } from "../../../services/geoapi";
 import { useState, useEffect } from "react";
 import './home.css';
+import { RxLetterCaseUppercase } from "react-icons/rx";
+import { SiUnitednations } from "react-icons/si";
+import { MdOutlinePlace, MdAttachMoney } from "react-icons/md";
+import { IoEarthSharp } from "react-icons/io5";
+import { FaRegMoneyBill1 } from "react-icons/fa6";
 
 function Home() {
 
-    const [countries, setCountries] = useState([]);
-    const [flags, setFlags]         = useState([]);
+    const [countries, setCountries]             = useState([]);
+    const [countrySelected, setCountrySelected] = useState({})
+    const [title, setTitle]                     = useState('Selecione o país desejado')
 
     useEffect( () => {
         async function loadCountries()
@@ -14,7 +20,7 @@ function Home() {
                 const response = await geoapi.get('countries?limit=9000')
                 const countriesWithFlags = response.data.countries.map(country => ({
                     ...country,
-                    flag: `https://data.geoapi.info/flags/1x1/${country.code.toLowerCase()}.svg`
+                    flag: getFlagUrl(country.code.toLowerCase())
                 }))
                
                 setCountries(countriesWithFlags)
@@ -27,26 +33,55 @@ function Home() {
         loadCountries()
     },[] )
 
+    async function selectCountry(name, code)
+    {
+        const response = await geoapi.get(`countries/search?q=${name}`)
+        setCountrySelected({
+            code: response.data.results[0].code,
+            name: response.data.results[0].name,
+            capitalName: response.data.results[0].capitalCity,
+            continent: response.data.results[0].continent,
+            currentCode: response.data.results[0].currencyCode,
+            currentName: response.data.results[0].currencyName,
+            flag: getFlagUrl(response.data.results[0].code),
+        })
+
+        setTitle(`${response.data.results[0].name} - ${response.data.results[0].code}`)
+    }
+
     return(
 
         <div className="container">
 
             <div className="countryInfoSelected">
                 <div className="title">
-                    <h1>Selecione o país desejado</h1>
+                    <h1>{title}</h1>
                 </div>
                 <div className="sideContainer">
-                    <div className="sideLeft">A</div>
-                    <div className="sideRight">B</div>
+                    
+                    <div className="sideLeft side">
+                        {countrySelected.code ? <span> <RxLetterCaseUppercase /><p>{countrySelected.code}</p></span> : ''}
+                        {countrySelected.name ? <span> <SiUnitednations /><p>{countrySelected.name}</p></span> : ''}
+                        {countrySelected.capitalName ? <span> <MdOutlinePlace /><p>{countrySelected.capitalName}</p></span> : ''}
+                        {countrySelected.continent ? <span> <IoEarthSharp /><p>{countrySelected.continent}</p></span> : ''}
+                        {countrySelected.currentCode ? <span> <MdAttachMoney /><p>{countrySelected.currentCode}</p></span> : ''}
+                        {countrySelected.currentName ? <span> <FaRegMoneyBill1 /><p>{countrySelected.currentName}</p></span> : ''}
+                    </div>
+                    
+                    <div className="sideRight side">
+                        <img src={countrySelected.flag} alt="" />
+                        <p>{countrySelected.name} - {countrySelected.code}</p>
+                    </div>
+                    
                 </div>
             </div>
 
-            <div className="teste">
+            <div className="contaienrFlags">
                 {
                     countries.map( (country, idx) => {
                         return(
                             <div className="containerCard" key={idx}>
-                                <div className="flag">
+                                <div className="flag" onClick={ () => selectCountry(country.name, country.code) }>
                                     <img src={country.flag} alt={country.code} title={country.name} />
                                 </div>
                             </div>
